b91e7abdeaf7c3316217304f3d4f57cc3ae008fd
wrathematics <wrathematics@gmail.com>
2015-12-10 15:27:13 -0500

    remove broken fit.term arg

--- R/figlet.r
+++ R/figlet.r
@@ -9,10 +9,6 @@
 #' @param smush 
 #' logical; whether or not the kerning should be "extreme" (TRUE)
 #' or "normal" (FALSE).
-#' @param fit.term 
-#' logical; if \code{TRUE}, then the printing will fit your
-#' terminal width, and otherwise will assume 80 columns.  In gui's like
-#' RStudio, this will probably do nothing. Does not work on Windows or Mac.
 #' @param respect.linebreaks 
 #' logical; if \code{TRUE}, then linebreaks will be
 #' printed as linebreaks, and otherwise as spaces.
@@ -30,16 +26,14 @@
 #' @importFrom utils capture.output
 #' 
 #' @export
-figlet <- function(message, font="standard", smush=TRUE, fit.term=TRUE, respect.linebreaks=FALSE)
+figlet <- function(message, font="standard", smush=TRUE, respect.linebreaks=FALSE)
 {
   must.be(message, "character")
   must.be(font, "character")
   must.be(smush, "logical")
-  must.be(fit.term, "logical")
   must.be(respect.linebreaks, "logical")
   
   mustnt.be(smush, "na")
-  mustnt.be(fit.term, "na")
   mustnt.be(respect.linebreaks, "na")
   
   font <- match.arg(font, .__Rfiglet_fonts)
@@ -48,21 +42,17 @@ figlet <- function(message, font="standard", smush=TRUE, fit.term=TRUE, respect.
   
   if (!smush) smusharg <- "-k"
   if (!respect.linebreaks) lbarg <- "-p"
-  if (get.os() == "windows" || get.os() == "darwin") fit.term <- FALSE # doesn't work on Windows
-  if (fit.term) termarg <- "-t"
   
   
   ### This is exactly what it looks like.
   argv <- c("./figlet", #"-c",
             if (!smush) smusharg, 
-            if (fit.term) termarg, 
             if (!respect.linebreaks) lbarg,
             "-d", fontdir,
             "-f", font, 
             as.character(message))
   
   ret <- capture.output(invisible(.Call(R_figlet_main, argv, PACKAGE="Rfiglet")))
-  
   class(ret) <- "figlet"
   
   return(ret)
